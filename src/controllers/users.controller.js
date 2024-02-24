7e09fe1c26e18245eafd0de62366f767693b3475
arni <89950723+ArniFuentes@users.noreply.github.com>
2024-02-24 14:53:20 -0300

    Agregar { session: false } al endpoint /users al ir con post

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -6,6 +6,7 @@ const router = Router();
 
 router.get(
   "/",
+  // Endpoint protegido
   passport.authenticate("jwt", { session: false }),
   async (req, res) => {
     try {
@@ -18,15 +19,19 @@ router.get(
   }
 );
 
-router.post("/", passport.authenticate("register"), async (req, res) => {
-  try {
-    // Se creó el recurso en la base de datos
-    res.status(201).json({ status: "success", message: "registered user" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ status: "Error", error: "Internal Server Error" });
+router.post(
+  "/",
+  passport.authenticate("register", { session: false }),
+  async (req, res) => {
+    try {
+      // Se creó el recurso en la base de datos
+      res.status(201).json({ status: "success", message: "registered user" });
+    } catch (error) {
+      console.log(error);
+      res.status(500).json({ status: "Error", error: "Internal Server Error" });
+    }
   }
-});
+);
 
 // router.get("/fail-register", (req, res) => {
 //   console.log("Falló registro");
