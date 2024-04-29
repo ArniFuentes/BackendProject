da1a1b96f898b6a19e09bc8c5cac611fa6494822
arni <89950723+ArniFuentes@users.noreply.github.com>
2024-04-29 17:24:10 -0400

    Agregar testing v6

--- tests/endpoints.test.js
+++ tests/endpoints.test.js
@@ -2,12 +2,13 @@ import supertest from "supertest";
 import { expect } from "chai";
 import config from "../src/configs/config.js";
 
-
 const requester = supertest("http://localhost:8080");
 
-describe("Test: crear un producto en la base como usuario admin o premium autenticado", () => {
-    let cookie;
-    it("Debería autenticar correctamente a un usuario", async function () {
+describe("Funcionalidades de la aplicación", () => {
+  let cookie;
+
+  describe("Test: Autenticar a un usuario", () => {
+    it("Debería autenticar al usuario al utilizar POST a /auth/login", async () => {
       const user = {
         email: config.emailUser,
         password: "123",
@@ -16,8 +17,12 @@ describe("Test: crear un producto en la base como usuario admin o premium autent
       const listWithTheCookie = response.headers["set-cookie"];
       const onlyTheCookie = listWithTheCookie[0];
       cookie = onlyTheCookie;
+
+      expect(response.status).to.equal(200);
     });
-  
+  });
+
+  describe("Test: Crear un producto en la base como usuario admin o premium autenticado", () => {
     it("Debería crear el producto en la base", async () => {
       // Datos ficticios pasados por body
       const productMock = {
@@ -28,21 +33,24 @@ describe("Test: crear un producto en la base como usuario admin o premium autent
         stock: 29,
         category: "Electrónica",
       };
-  
-      await requester
+
+      const response = await requester
         .post("/api/products")
         .set("Cookie", cookie)
         .send(productMock);
+
+      // Asegúrate de que la creación del producto sea exitosa
+      expect(response.status).to.equal(201);
+      expect(response.body).to.have.property("status", "success");
     });
   });
-  
-  describe("Test: resporder un producto elegido", () => {
-    // Prueba para verificar si el endpoint GET api/products/:pid obtiene un producto correctamente
+
+  describe("Test: Responder un producto elegido", () => {
     it("Debería obtener un producto correctamente al llamar a GET api/products/:pid", async () => {
       // Supongamos que pid es un ID válido de un producto existente en tu base de datos
       const productId = "65f5c84eff6b5caf6ca28b95";
       const response = await requester.get(`/api/products/${productId}`);
-  
+
       // Verificar que la respuesta sea exitosa (código de estado 200)
       expect(response.status).to.equal(200);
       // Verificar que el cuerpo de la respuesta tenga una propiedad llamada "status" con el valor "success"
@@ -54,43 +62,27 @@ describe("Test: crear un producto en la base como usuario admin o premium autent
     });
   });
 
-  describe("Test: Agregar un producto a un determinado carrito", () => {
-    let cookie;
-    it("Debería autenticar correctamente a un usuario", async function () {
-      const user = {
-        email: config.emailUser,
-        password: "123",
-      };
-      const response = await requester.post("/auth/login").send(user);
-      const listWithTheCookie = response.headers["set-cookie"];
-      const onlyTheCookie = listWithTheCookie[0];
-      cookie = onlyTheCookie;
-    });
-  
+  describe("Test: Agregar un producto a un determinado carrito como usuario autenticado", () => {
     it("Debería agregar el producto al carro", async () => {
-      await requester
+      const response = await requester
         .post(
           "/api/carts/662d8f4a97a6100913718a71/product/65f5150f5bb996864dbcc3b2"
         )
         .set("Cookie", cookie);
+
+      expect(response.body).to.have.property(
+        "message",
+        "Producto agregado al carrito exitosamente."
+      );
     });
   });
 
   describe("Test: Enviar la información del usuario que está autenticado", () => {
-    let cookie;
-    it("Debería autenticar correctamente a un usuario", async function () {
-      const user = {
-        email: config.emailUser,
-        password: "123",
-      };
-      const response = await requester.post("/auth/login").send(user);
-      const listWithTheCookie = response.headers["set-cookie"];
-      const onlyTheCookie = listWithTheCookie[0];
-      cookie = onlyTheCookie;
-    });
-  
     it("Debería enviar la información del usuario que está autenticado", async () => {
-      await requester.get("/api/sessions/current");
+      const response = await requester.get("/api/sessions/current");
+      console.log(response.body);
+
+      // expect(response.status).to.equal(200);
     });
   });
-  
+});
