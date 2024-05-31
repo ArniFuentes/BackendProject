87961fead7dadb3cf74e0732d3667d79a14183cf
arni <89950723+ArniFuentes@users.noreply.github.com>
2024-05-31 02:27:47 -0400

    Agregar cambio v26

--- src/db/index.js
+++ src/db/index.js
@@ -5,8 +5,7 @@ import config from "../configs/config.js";
 const mongoConnect = async () => {
   try {
     await mongoose.connect(
-      // `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}?retryWrites=true&w=majority&appName=Cluster0`
-      "mongodb://localhost:27017/test_coder"
+      `mongodb+srv://${config.dbUser}:${config.dbPassword}@${config.dbHost}/${config.dbName}?retryWrites=true&w=majority&appName=Cluster0`
     );
     logger.info("DB is connected");
   } catch (error) {
